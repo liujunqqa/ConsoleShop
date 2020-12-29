7e24dd22ca5b58edf60c25cff5455ae95118388e
mengxiangni <1666878496@qq.com>
2020-12-29 20:34:17 +0800

    加入购物车

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -10,12 +10,12 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class ReadProductExcel {
-    public Product[] readProductExcel(InputStream kl) {
-        Product products[] = null;
+    public Product[] readProductExcel( InputStream kl) {
+        Product products[]=null;
         try {
             XSSFWorkbook xw = new XSSFWorkbook(kl);
             XSSFSheet xs = xw.getSheetAt(0);
-            products = new Product[xs.getLastRowNum()];
+            products=new Product[xs.getLastRowNum()];
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
                 XSSFRow row = xs.getRow(j);
                 Product product = new Product();//每循环一次就把电子表格的一行的数据给对象赋值
@@ -33,13 +33,45 @@ public class ReadProductExcel {
                         product.setMs(this.getValue(cell));//给phone属性赋值
                     }
                 }
-                products[j-1] = product;
+               products[j-1]=product;
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
         return products;
+    }
+
+    public Product readProductId( String vb,InputStream kl) {
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(kl);
+            XSSFSheet xs = xw.getSheetAt(0);
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j);
+                Product product = new Product();//每循环一次就把电子表格的一行的数据给对象赋值
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k);
+                    if (cell == null)
+                        continue;
+                    if (k == 0) {
+                        product.setId(this.getValue(cell));//给username属性赋值
+                    } else if (k == 1) {
+                        product.setName(this.getValue(cell));//给password属性赋值
+                    } else if (k == 2) {
+                        product.setPricr(Float.valueOf(this.getValue(cell)));//给address属性赋值
+                    } else if (k == 3) {
+                        product.setMs(this.getValue(cell));//给phone属性赋值
+                    }
+                }
+                if(vb.equals(product.getId())){
+                    return product;
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private String getValue(XSSFCell cell) {
--- src/Test.java
+++ src/Test.java
@@ -13,7 +13,6 @@ public class Test {
             InputStream kl = Class.forName("Test").getResourceAsStream("/User.xlsx");
             ReadeUserExcel readeExcel = new ReadeUserExcel();
             User user[] = readeExcel.readExcel(kl);
-
             for (int i = 0; i < user.length; i++) {
                 if (user[i].getUsername().equals(username) && user[i].getPassword().equals(password)) {
                     l = 1;
@@ -25,14 +24,23 @@ public class Test {
                 InputStream k2 = Class.forName("Test").getResourceAsStream("/Product.xlsx");
                 Product[]products=readProductExcel.readProductExcel(k2);
                 for(int i=0;i<products.length;i++){
-                    System.out.print("商品id"+products[i].getId()+"\t");
-                    System.out.print("商品name"+products[i].getName()+"\t");
-                    System.out.print("商品price"+products[i].getPricr()+"\t");
+                    System.out.print("商品编号"+products[i].getId()+"\t");
+                    System.out.print("商品名"+products[i].getName()+"\t");
+                    System.out.print("商品价格"+products[i].getPricr()+"\t");
                     System.out.println("商品描述"+products[i].getMs()+"\t");
                 }
-                System.out.println("请选择相应的商品进行购买");
-
-
+                int count=0;
+                System.out.println("请输入商品的ID进行购买");
+                String vb=in.next();
+                ReadeUserExcel readeUserExcel1=new ReadeUserExcel();
+                k2=null;
+                k2=Class.forName("Test").getResourceAsStream("/Product.xlsx");
+                Product product=readProductExcel.readProductId(vb,k2);
+                if(product!=null){
+                    Product product1[]=new Product[4];
+                    product1[count]=product;
+                    count=count+1;
+                }
             } else {
                 System.out.println("登录失败");
             }
