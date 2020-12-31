b2ce278dad43728ec0d5e414d087b0189572ca90
mengxiangni <1666878496@qq.com>
2020-12-31 21:42:18 +0800

    加入购物车

--- src/Test.java
+++ src/Test.java
@@ -24,22 +24,56 @@ public class Test {
                 InputStream k2 = Class.forName("Test").getResourceAsStream("/Product.xlsx");
                 Product[]products=readProductExcel.readProductExcel(k2);
                 for(int i=0;i<products.length;i++){
-                    System.out.print("商品编号"+products[i].getId()+"\t");
-                    System.out.print("商品名"+products[i].getName()+"\t");
-                    System.out.print("商品价格"+products[i].getPricr()+"\t");
-                    System.out.println("商品描述"+products[i].getMs()+"\t");
+                    System.out.print("商品编号:"+products[i].getId()+"\t");
+                    System.out.print("商品名:"+products[i].getName()+"\t");
+                    System.out.print("商品价格:"+products[i].getPricr()+"\t");
+                    System.out.println("商品描述:"+products[i].getMs()+"\t");
                 }
-                int count=0;
-                System.out.println("请输入商品的ID进行购买");
-                String vb=in.next();
-                ReadeUserExcel readeUserExcel1=new ReadeUserExcel();
-                k2=null;
-                k2=Class.forName("Test").getResourceAsStream("/Product.xlsx");
-                Product product=readProductExcel.readProductId(vb,k2);
-                if(product!=null){
-                    Product product1[]=new Product[4];
-                    product1[count]=product;
-                    count=count+1;
+                boolean h=true;
+                int count = 0;
+                Product product1[] = new Product[4];
+                while (h) {
+                    System.out.println("请输入商品的ID进行购买");
+                    String vb = in.next();
+                    ReadeUserExcel readeUserExcel1 = new ReadeUserExcel();
+                    k2 = null;
+                    k2 = Class.forName("Test").getResourceAsStream("/Product.xlsx");
+                    Product product = readProductExcel.readProductId(vb, k2);
+                    if (product != null) {
+                        product1[count] = product;
+                        count = count + 1;
+                        System.out.println("加入成功，选择下列操作:1.继续加入 2.查看购物车 3.退出");
+                        int answer=in.nextInt();
+                        if(answer==1){
+                            for(int i=0;i<products.length;i++){
+                                System.out.print("商品编号"+products[i].getId()+"\t");
+                                System.out.print("商品名"+products[i].getName()+"\t");
+                                System.out.print("商品价格"+products[i].getPricr()+"\t");
+                                System.out.println("商品描述"+products[i].getMs()+"\t");
+                            }
+
+                        }
+                        else if(answer==2){
+                            for(int i=0;i<count;i++) {
+                                System.out.print(product1[i].getId() + "\t");
+                                System.out.print(product1[i].getName() + "\t");
+                                System.out.print(product1[i].getPricr() + "\t");
+                                System.out.println(product1[i].getMs() + "\t");
+                            }
+                                System.out.println("查询成功，是否继续购买：1.是 2.否");
+                                int b=in.nextInt();
+                                if(b==1){
+                                    answer=1;
+                                }
+                                else if(b==2){
+                                    answer=3;
+                                }
+
+                        }
+                        else if (answer==3){
+                            h=false;
+                        }
+                    }
                 }
             } else {
                 System.out.println("登录失败");
